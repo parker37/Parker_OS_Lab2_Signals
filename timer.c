83db7decc429a6a5e2a737f68379e523dc3e5c99
Ronte' Parker <ronte.parker@bison.howard.edu>
2023-01-14 00:44:20 +0000

    initial setup of lab2

---
+++ timer.c
@@ -0,0 +1,14 @@
+/* timer.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+//****
+
+COPY YOUR SIGNAL.C INTO THIS FILE
+
+UPDATE YOUR MAKEFILE
+
+
+*******/
