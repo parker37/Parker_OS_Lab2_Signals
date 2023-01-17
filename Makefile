65be32e461fd7b73ee6e805f0255bc921937bb4d
Ronte' Parker <ronte.parker@bison.howard.edu>
2023-01-17 20:26:11 +0000

    Timer.c file sigint status message (Assignment 3) finished and makefile updated

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+timers: timer.c
+	gcc timer.c -o timer
+
 signals: signal.c
-	gcc signal.c -o signal 
+	gcc signal.c -o signal
--- timer.c
+++ timer.c
@@ -1,14 +1,43 @@
 /* timer.c */
+/* hello_signal.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
+#include <time.h>
 
-//****
+#define MAXSIZE 100
+int alarms = 0;
+time_t start, end, total;
+char buf[MAXSIZE];
 
-COPY YOUR SIGNAL.C INTO THIS FILE
+void handler(int signum) { //signal handler
+  printf("Hello World!\n");
+  alarms += 1;
+  //exit(1); //exit after printing
+}
 
-UPDATE YOUR MAKEFILE
+void stats(int signum) { //signal handler
+  end = time(NULL);
+  total = end - start;
+  printf("\nNumber of Alarms: %d\n", alarms);
+  printf("Total program runtime: %ld seconds\n", total);
+  exit(1);
+}
 
+int main(int argc, char * argv[])
+{
+  start = time(NULL);
+  signal(SIGALRM, handler); //register handler to handle SIGALRM
+  signal(SIGINT, stats); //register handler to handle SIGINT
 
-*******/
+  while(1) {
+    alarm(1); //Schedule a SIGALRM for 1 second 
+    sleep(1);
+    sprintf(buf, "Turing was right!\n");
+    write(1, buf, strlen(buf));
+
+  } //busy wait for signal to be delivered
+  return 0; //never reached
+}
